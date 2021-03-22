be464958f07d73a98c1ba7a3c6ad17ec4196f3de
Drassero <taghi.adnane@gmail.com>
2021-03-22 22:50:01 +0100

    Project Init

---
+++ src/main/java/com/drassero/amazed/Main.java
@@ -0,0 +1,17 @@
+package com.drassero.amazed;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		System.out.println("Hello World !");
+	}
+	
+    public static void main(String[] args) {
+    	launch(args);
+    }
+
+}
